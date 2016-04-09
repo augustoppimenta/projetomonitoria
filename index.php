810de98ea3ec37a7cdc4747b6ca0d2f8f8cbbb80
augusto pimenta <augustoppimenta@gmail.com>
2016-04-09 12:15:08 -0300

    Inserindo JS e editando nome da sucesso.php

--- index.php
+++ index.php
@@ -9,26 +9,30 @@
    </head>
    <body>
     <?php include("menu.php") ?>
-    <div class="container">
+      <div class="container">
         <div class="jumbotron">
             <h1>Bem vindo ao sistema BW!</h1>
-	<p>Esse é um trabalho desenvolvido na disciplina de Banco de Dados Web - UFRN</p>
+	       <p>Esse é um trabalho desenvolvido na disciplina de Banco de Dados Web - UFRN</p>
         </div>
         <div class="panel panel-default" class="col-lg-6">
             <div class="panel-heading">LOGIN</div>
-	   <div class="panel-body">
-	       <form class="form-login" action="login.php" method="POST">
+	       <div class="panel-body">
+	         <form class="form-login" action="login.php" method="POST">
 	           Email</br>
 	           <input type="text" name="email" value=""></br>
 	           Senha</br>
    	           <input type="password" name="senha" value=""></br>
 	           <input class="btn btn-primary" type="submit" value="Entrar">
-	       </form>
-	       Nao possui conta?<a href="cadastro.php">Cadastre-se agora</a>
-	   </div>
-	</div>
-            </div>
+	         </form>
+	         Nao possui conta?<a href="cadastro.php">Cadastre-se agora</a>
+	       </div>
+	     </div>
+          </div>
         </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
    </body>
 </html>
 
---
+++ sucesso.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+   <head>
+        <title>Seu nome Home Page</title>
+        <meta charset="utf-8">
+        <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+   </head>
+   <body>
+        <?php include("menu.php") ?>
+        <h1>Sucesso</h1>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+   </body>
+</html>
