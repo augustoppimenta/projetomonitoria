fb023fb1a2035ed1757669a8dfe696e34dc3e239
augusto pimenta <augustoppimenta@gmail.com>
2016-04-09 13:21:04 -0300

    Configurando tags

---
+++ perfil.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+   <head>
+        <title>Home</title>
+        <meta charset="utf-8">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+   </head>
+   <body>
+        <?php include("menu_interno.php") ?>
+        <div class="container">
+            <div class="fb-profile">
+                <img align="left" class="fb-image-lg" src="http://lorempixel.com/850/280/nightlife/5/" alt="Profile image example"/>
+                <img align="left" class="fb-image-profile thumbnail" src="http://lorempixel.com/180/180/people/9/" alt="Profile image example"/>
+                <div class="fb-profile-text">
+                    <h1>Eli Macy</h1>
+                    <p>Girls just wanna go fun.</p>
+                </div>
+            </div>
+        </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+   </body>
+</html>
