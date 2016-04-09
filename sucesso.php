1687e811be2f8aacfef05b51b6db0cfcc16503b1
augusto pimenta <augustoppimenta@gmail.com>
2016-04-09 12:48:35 -0300

    Retirango tags desnecessárias

--- sucesso.php
+++ sucesso.php
@@ -8,17 +8,13 @@
         <link rel="stylesheet" type="text/css" href="css/estilo.css">
    </head>
    <body>
-    <section>
         <?php include("menu_interno.php") ?>
-    </section>
-    <section>
-            <div class="jumbotron">
-                <div class="container">
-                    <h1>Bem vindo ao sistema BW!</h1>
-                    <p>Agora você já pode tirar suas dúvidas.</p>
-                </div>
+        <div class="jumbotron">
+            <div class="container">
+                <h1>Bem vindo ao sistema BW!</h1>
+                <p>Agora você já pode tirar suas dúvidas.</p>
             </div>
-    </section>
+        </div>
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
